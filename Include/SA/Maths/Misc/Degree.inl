9cc01d08048a36ab3f2310cc26d1485bb7a8b57a
mrouffet <maximerouffet@gmail.com>
2022-01-06 12:53:45 +0100

    [Add] Degree

---
+++ Include/SA/Maths/Misc/Degree.inl
@@ -0,0 +1,148 @@
+// Copyright 2022 Sapphire development team.All Rights Reserved.
+
+namespace Sa
+{
+	template <typename T>
+	constexpr Deg<T>::Deg(T _deg) noexcept : mHandle{ _deg }
+	{
+	}
+	
+	template <typename T>
+	template <typename TIn>
+	constexpr Deg<T>::Deg(Deg<TIn> _other) noexcept : mHandle{ static_cast<T>(_other.Handle()) }
+	{
+	}
+
+	template <typename T>
+	template <typename TIn>
+	constexpr Deg<T>::Deg(Rad<TIn> _rad) noexcept : mHandle{ static_cast<T>(_rad.Handle() * Maths::RadToDeg) }
+	{
+	}
+
+
+	template <typename T>
+	constexpr T Deg<T>::Handle() const noexcept
+	{
+		return mHandle;
+	}
+
+	template <typename T>
+	constexpr bool Deg<T>::Equals(Deg<T> _other, T _epsilon) const noexcept
+	{
+		return std::abs(mHandle - _other.mHandle) <= _epsilon;
+	}
+
+	template <typename T>
+	void Deg<T>::Clamp() noexcept
+	{
+		mHandle = std::fmod(mHandle, static_cast<T>(360));
+
+		if (mHandle < -static_cast<T>(180))
+			mHandle += static_cast<T>(360);
+		else if(mHandle > static_cast<T>(180))
+			mHandle -= static_cast<T>(360);
+	}
+
+
+	template <typename T>
+	constexpr Deg<T> Deg<T>::operator-() const noexcept
+	{
+		return -mHandle;
+	}
+
+	template <typename T>
+	constexpr Deg<T> Deg<T>::operator+(Deg _rhs) const noexcept
+	{
+		return mHandle + _rhs.mHandle;
+	}
+
+	template <typename T>
+	constexpr Deg<T> Deg<T>::operator-(Deg _rhs) const noexcept
+	{
+		return mHandle - _rhs.mHandle;
+	}
+
+	template <typename T>
+	constexpr Deg<T> Deg<T>::operator*(T _scale) const noexcept
+	{
+		return Deg(mHandle * _scale);
+	}
+
+	template <typename T>
+	Deg<T> Deg<T>::operator/(T _scale) const
+	{
+		//SA_WARN(!Sa::Equals0(_scale), Maths, L"Unscale Degree by 0 (division by 0).");
+
+		return Deg(mHandle / _scale);
+	}
+
+
+	template <typename T>
+	Deg<T>& Deg<T>::operator+=(Deg _rhs) noexcept
+	{
+		mHandle += _rhs.mHandle;
+
+		return *this;
+	}
+
+	template <typename T>
+	Deg<T>& Deg<T>::operator-=(Deg _rhs) noexcept
+	{
+		mHandle -= _rhs.mHandle;
+
+		return *this;
+	}
+
+	template <typename T>
+	Deg<T>& Deg<T>::operator*=(T _scale) noexcept
+	{
+		mHandle *= _scale;
+
+		return *this;
+	}
+
+	template <typename T>
+	Deg<T>& Deg<T>::operator/=(T _scale)
+	{
+		//SA_WARN(!Sa::Equals0(_scale), Maths, L"Unscale Deg<" << typeid(T).name() << "> by 0 (division by 0).");
+
+		mHandle /= _scale;
+
+		return *this;
+	}
+
+
+	template <typename T>
+	constexpr bool Deg<T>::operator==(Deg<T> _rhs) const noexcept
+	{
+		return Equals(_rhs);
+	}
+
+	template <typename T>
+	constexpr bool Deg<T>::operator!=(Deg<T> _rhs) const noexcept
+	{
+		return !Equals(_rhs);
+	}
+
+
+#if SA_LOGGING
+
+	template <typename T>
+	std::string Deg<T>::ToString() const noexcept
+	{
+		return std::to_string(mHandle) + "_deg";
+	}
+
+#endif
+
+
+	constexpr Degf operator""_deg(uint64_t _lit) noexcept
+	{
+		return Degf(static_cast<float>(_lit));
+	}
+
+	constexpr Degd operator""_deg(long double _lit) noexcept
+	{
+		return Degd(static_cast<double>(_lit));
+	}
+}
---
+++ Tests/UnitTests/Misc/DegreeTests.cpp
@@ -0,0 +1,176 @@
+// Copyright 2022 Sapphire development team.All Rights Reserved.
+
+#include <gtest/gtest.h>
+
+#include <SA/Maths/Misc/Degree.hpp>
+#include <SA/Maths/Misc/Radian.hpp>
+
+namespace Sa::UT::Degree
+{
+	template <typename T>
+	class DegreeTest : public testing::Test
+	{
+	};
+
+	using TestTypes = ::testing::Types<float, double>;
+	TYPED_TEST_SUITE(DegreeTest, TestTypes);
+
+	TYPED_TEST(DegreeTest, Constructors)
+	{
+		const Deg<TypeParam> d1;
+		EXPECT_EQ(d1.Handle(), 0);
+
+		const TypeParam d2_val = TypeParam{ 2.56 };
+		const Deg<TypeParam> d2 = d2_val;
+		EXPECT_EQ(d2.Handle(), d2_val);
+
+		const Rad<TypeParam> r4 = TypeParam{ Maths::Pi };
+		const Deg<TypeParam> d4 = r4;
+		EXPECT_EQ(d4.Handle(), 180);
+	}
+
+	TEST(Degree, CastConstructor)
+	{
+		const Degd dd1 = 4.23;
+		const Degf d1 = dd1;
+		EXPECT_EQ(d1.Handle(), 4.23f);
+
+		const Degf df2 = 6.235f;
+		const Degd d2 = df2;
+		EXPECT_EQ(d2.Handle(), double{ 6.235f });
+	}
+
+	TYPED_TEST(DegreeTest, Equals)
+	{
+		const Deg<TypeParam> d1 = TypeParam{ 1.3 };
+		const Deg<TypeParam> d2 = TypeParam{ 4.0 };
+		const Deg<TypeParam> d3 = TypeParam{ 1.3 } + std::numeric_limits<TypeParam>::epsilon();
+
+		EXPECT_FALSE(d1.Equals(d2));
+		EXPECT_TRUE(d1.Equals(d1));
+		EXPECT_TRUE(d1.Equals(d3));
+
+		EXPECT_FALSE(d1 == d2);
+		EXPECT_TRUE(d1 == d1);
+		EXPECT_TRUE(d1 == d3);
+
+		EXPECT_TRUE(d1 != d2);
+		EXPECT_FALSE(d1 != d1);
+		EXPECT_FALSE(d1 != d3);
+	}
+
+	TYPED_TEST(DegreeTest, Clamp)
+	{
+		const TypeParam d1_val = TypeParam{ 10.0 };
+		Deg<TypeParam> d1 = d1_val;
+		d1.Clamp();
+		EXPECT_EQ(d1, d1_val);
+
+		const TypeParam d2_val = TypeParam{ 310.0 };
+		const TypeParam d2_clampedVal = TypeParam{ -50.0 };
+		Deg<TypeParam> d2 = d2_val;
+		d2.Clamp();
+		EXPECT_EQ(d2, d2_clampedVal);
+
+		const TypeParam d3_val = TypeParam{ 670.0 };
+		const TypeParam d3_clampedVal = TypeParam{ -50.0 };
+		Deg<TypeParam> d3 = d3_val;
+		d3.Clamp();
+		EXPECT_EQ(d3, d3_clampedVal);
+
+		const TypeParam d4_val = TypeParam{ -200.0 };
+		const TypeParam d4_clampedVal = TypeParam{ 160.0 };
+		Deg<TypeParam> d4 = d4_val;
+		d4.Clamp();
+		EXPECT_EQ(d4, d4_clampedVal);
+
+		const TypeParam d5_val = TypeParam{ -560.0 };
+		const TypeParam d5_clampedVal = TypeParam{ 160.0 };
+		Deg<TypeParam> d5 = d5_val;
+		d5.Clamp();
+		EXPECT_EQ(d5, d5_clampedVal);
+	}
+
+	TYPED_TEST(DegreeTest, OperatorSelfMinus)
+	{
+		const Deg<TypeParam> d1 = TypeParam{ 180.0 };
+
+		EXPECT_EQ(-d1, TypeParam{ -180.0 });
+
+		// Ensure correct conversion.
+		const Rad<TypeParam> r1 = -d1;
+		EXPECT_EQ(r1, TypeParam{ -Maths::Pi });
+	}
+
+	TYPED_TEST(DegreeTest, OperatorPlus)
+	{
+		const Deg<TypeParam> d1 = TypeParam{ 160.0 };
+		const Deg<TypeParam> d2 = TypeParam{ 20.0 };
+
+		EXPECT_EQ(d1 + d2, TypeParam{ 180.0 });
+
+		// Ensure correct conversion.
+		const Rad<TypeParam> r1 = d1 + d2;
+		EXPECT_EQ(r1, TypeParam{ Maths::Pi });
+
+		Deg<TypeParam> d3 = d1;
+		d3 += d2;
+		EXPECT_EQ(d3, d1 + d2);
+	}
+
+	TYPED_TEST(DegreeTest, OperatorMinus)
+	{
+		const Deg<TypeParam> d1 = TypeParam{ 200.0 };
+		const Deg<TypeParam> d2 = TypeParam{ 20.0 };
+
+		EXPECT_EQ(d1 - d2, TypeParam{ 180.0 });
+
+		// Ensure correct conversion.
+		const Rad<TypeParam> r1 = d1 - d2;
+		EXPECT_EQ(r1, TypeParam{ Maths::Pi });
+
+		Deg<TypeParam> d3 = d1;
+		d3 -= d2;
+		EXPECT_EQ(d3, d1 - d2);
+	}
+
+	TYPED_TEST(DegreeTest, OperatorScale)
+	{
+		const Deg<TypeParam> d1 = TypeParam{ 90.0 };
+
+		EXPECT_EQ(d1 * 2, TypeParam{ 180.0 });
+
+		// Ensure correct conversion.
+		const Rad<TypeParam> r1 = d1 * 2;
+		EXPECT_EQ(r1, TypeParam{ Maths::Pi });
+
+		Deg<TypeParam> d3 = d1;
+		d3 *= 2;
+		EXPECT_EQ(d3, d1 * 2);
+	}
+
+	TYPED_TEST(DegreeTest, OperatorUnScale)
+	{
+		const Deg<TypeParam> d1 = TypeParam{ 180.0 };
+
+		EXPECT_EQ(d1 / 2, TypeParam{ 90.0 });
+
+		// Ensure correct conversion.
+		const Rad<TypeParam> r1 = d1 / 2;
+		EXPECT_EQ(r1, TypeParam{ Maths::PiOv2 });
+
+		Deg<TypeParam> d3 = d1;
+		d3 /= 2;
+		EXPECT_EQ(d3, d1 / 2);
+	}
+
+
+	TEST(Degree, Literals)
+	{
+		const Degf d1 = 4_deg;
+		EXPECT_EQ(d1.Handle(), 4.0f);
+
+		const Degd d2 = 1.3_deg;
+		EXPECT_EQ(d2.Handle(), 1.3);
+	}
+}
