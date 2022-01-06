0683a380b1a157dc8356617219d4d3dba31278d9
mrouffet <maximerouffet@gmail.com>
2022-01-06 09:55:26 +0100

    [Add] Config file

---
+++ Include/SA/Maths/Config.hpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2022 Sapphire's Suite. All Rights Reserved.
+
+#pragma once
+
+#ifndef SAPPHIRE_MATHS_CONFIG_GUARD
+#define SAPPHIRE_MATHS_CONFIG_GUARD
+
+/**
+*	\file Maths/Config.hpp
+*
+*	\brief Maths specific config file.
+*
+*	\ingroup Maths
+*	\{
+*/
+
+
+/**
+*	\defgroup Maths Maths
+*	Sapphire Suite's Maths Module.
+*/
+
+/**
+*	\defgroup Maths_Algorithms Algorithms
+*	Sapphire Suite's Maths Algorithms.
+*	\ingroup Maths
+*/
+
+/**
+*	\defgroup Maths_Space Space
+*	Sapphire Suite's Maths Space.
+*	\ingroup Maths
+*/
+
+/**
+*	\defgroup Maths_Misc Misc
+*	Sapphire Suite's Maths Misc.
+*	\ingroup Maths
+*/
+
+
+/** \} */
+
+#endif // GUARD
