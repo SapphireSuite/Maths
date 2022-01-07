3d11d099b026229d32b6774f4573743e12d3b23d
mrouffet <maximerouffet@gmail.com>
2022-01-07 12:22:19 +0100

    [Add] Tan method

---
+++ Tests/UnitTests/Algorithms/TanTests.cpp
@@ -0,0 +1,33 @@
+// Copyright 2022 Sapphire development team.All Rights Reserved.
+
+#include <gtest/gtest.h>
+
+#include <SA/Maths/Misc/Degree.hpp>
+#include <SA/Maths/Algorithms/Tan.hpp>
+
+namespace Sa::UT::Tan
+{
+	TEST(TrigoTest, TangentFloat)
+	{
+		// TODO: Comparison not working ?
+		//EXPECT_EQ(Maths::Tan<float>(Maths::Pi<float>), 0.0f);
+		EXPECT_EQ(Maths::Tan<float>(Maths::Pi<float>) - std::numeric_limits<float>::epsilon() <= 0.0f, true);
+		
+		EXPECT_FLOAT_EQ(Maths::Tan<float>(Maths::PiOv3<float>), float{ 1.7320508075688767 });
+		EXPECT_EQ(Maths::Tan<float>(-Maths::PiOv6<float>), float{ -0.57735026918962573 });
+		EXPECT_FLOAT_EQ(Maths::Tan<float>(40_deg), float{ 0.83909963117727993 });
+		EXPECT_FLOAT_EQ(Maths::Tan<float>(0.69813170079773179), float{ 0.83909963117727993 });
+	}
+
+	TEST(TrigoTest, TangentDouble)
+	{
+		// TODO: Comparison not working ?
+		//EXPECT_EQ(Maths::Tan<double>(Maths::Pi<double>), 0.0f);
+		EXPECT_EQ(Maths::Tan<double>(Maths::Pi<double>) - std::numeric_limits<double>::epsilon() <= 0.0f, true);
+
+		EXPECT_EQ(Maths::Tan<double>(Maths::PiOv3<double>), 1.7320508075688767);
+		EXPECT_EQ(Maths::Tan<double>(-Maths::PiOv6<double>), -0.57735026918962573);
+		EXPECT_EQ(Maths::Tan<double>(40_deg), 0.83909963117727993);
+		EXPECT_EQ(Maths::Tan<double>(0.69813170079773179), 0.83909963117727993);
+	}
+}
