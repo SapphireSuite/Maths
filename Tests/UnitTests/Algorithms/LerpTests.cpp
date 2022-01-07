f8b9143247b501ca5f4cf6c7493df0c29530c4fe
mrouffet <maximerouffet@gmail.com>
2022-01-07 14:05:25 +0100

    [Add] Lerp

---
+++ Tests/UnitTests/Algorithms/LerpTests.cpp
@@ -0,0 +1,47 @@
+// Copyright 2022 Sapphire development team.All Rights Reserved.
+
+#include <gtest/gtest.h>
+
+#include <SA/Maths/Algorithms/Equals.hpp>
+using namespace Sa;
+
+namespace Sa::UT::Lerp
+{
+	template <typename T>
+	class LerpTest : public testing::Test
+	{
+	};
+
+	using TestTypes = ::testing::Types<float, double>;
+	TYPED_TEST_SUITE(LerpTest, TestTypes);
+
+	TYPED_TEST(LerpTest, Lerp)
+	{
+		EXPECT_EQ(Maths::Lerp(TestTypes{ 1.0 }, TestTypes{ 5.0 }, 0.0f), TestTypes{ 1.0 });
+		EXPECT_EQ(Maths::Lerp(TestTypes{ 1.0 }, TestTypes{ 5.0 }, 1.0f), TestTypes{ 5.0 });
+		EXPECT_EQ(Maths::Lerp(TestTypes{ 1.0 }, TestTypes{ 5.0 }, 0.5f), TestTypes{ 3.0 });
+
+		EXPECT_EQ(Maths::Lerp(TestTypes{ 1.0 }, TestTypes{ 5.0 }, -1.0f), TestTypes{ 1.0 });
+		EXPECT_EQ(Maths::Lerp(TestTypes{ 1.0 }, TestTypes{ 5.0 }, 2.0f), TestTypes{ 5.0 });
+	}
+
+	TYPED_TEST(LerpTest, LerpUnclamped)
+	{
+		EXPECT_EQ(Maths::LerpUnclamped(TestTypes{ 1.0 }, TestTypes{ 5.0 }, 0.0f), TestTypes{ 1.0 });
+		EXPECT_EQ(Maths::LerpUnclamped(TestTypes{ 1.0 }, TestTypes{ 5.0 }, 1.0f), TestTypes{ 5.0 });
+		EXPECT_EQ(Maths::LerpUnclamped(TestTypes{ 1.0 }, TestTypes{ 5.0 }, 0.5f), TestTypes{ 3.0 });
+		EXPECT_EQ(Maths::LerpUnclamped(TestTypes{ 1.0 }, TestTypes{ 5.0 }, -1.0f), TestTypes{ -3.0 });
+		EXPECT_EQ(Maths::LerpUnclamped(TestTypes{ 1.0 }, TestTypes{ 5.0 }, 2.0f), TestTypes{ 9.0 });
+
+	}
+
+	TYPED_TEST(LerpTest, SLerp)
+	{
+		// TODO: Implement.
+	}
+
+	TYPED_TEST(LerpTest, SLerpUnclamped)
+	{
+		// TODO: Implement.
+	}
+}
