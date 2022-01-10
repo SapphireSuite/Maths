184d9b225739807d476f39302334c064f553e374
mrouffet <maximerouffet@gmail.com>
2022-01-10 23:42:40 +0100

    [Add] Do not optimize

--- Tests/Benchmark/Space/QuaternionBenchmark.cpp
+++ Tests/Benchmark/Space/QuaternionBenchmark.cpp
@@ -9,10 +9,11 @@ namespace Sa::Benchmark::Quaternion
     template <typename T>
     static void SqrLength(benchmark::State& _state)
     {
+        T x = T();
         const Sa::Quat<T> q(T{ 13.21 }, T{ 1.346 }, T{ 55.21 }, T{ 75.21 });
 
         for (auto _ : _state)
-            q.SqrLength();
+            benchmark::DoNotOptimize(x += q.SqrLength());
     }
 
     BENCHMARK_TEMPLATE(SqrLength, float);
