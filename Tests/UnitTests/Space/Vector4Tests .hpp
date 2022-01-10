3e5228df80ae1c1ab7733c0aa1d24c4e84b007d9
mrouffet <maximerouffet@gmail.com>
2022-01-10 15:41:41 +0100

    [Add] UT Vec4 near

--- Tests/UnitTests/Space/Vector4Tests .hpp
+++ Tests/UnitTests/Space/Vector4Tests .hpp
@@ -21,3 +21,14 @@ namespace Sa
 
 /// Google Test typedef helper.
 #define Vec4T Vec4<TypeParam>
+
+#define EXPECT_VEC4_NEAR(_v1, _v2, eps)\
+{\
+	auto v1V = (_v1);\
+	auto v2V = (_v2);\
+\
+	EXPECT_NEAR(v1V.x, v2V.x, eps);\
+	EXPECT_NEAR(v1V.y, v2V.y, eps);\
+	EXPECT_NEAR(v1V.z, v2V.z, eps);\
+	EXPECT_NEAR(v1V.w, v2V.w, eps);\
+}
