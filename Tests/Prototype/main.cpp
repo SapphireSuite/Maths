d4274f823fd2dbe060e21a88f98ddc4d6ac554e1
mrouffet <maximerouffet@gmail.com>
2022-05-09 14:55:13 +0900

    [Clean] proto main

--- Tests/Prototype/main.cpp
+++ Tests/Prototype/main.cpp
@@ -4,27 +4,7 @@
 
 #define LOG(_str) std::cout << _str << std::endl;
 
-#include <SA/Collections/Transform>
-using namespace Sa;
-
 int main()
 {
-	TrPRSf tr1 {
-		Vec3f(6.3f, 5.14f, 9.25f),
-		Quatf(40_deg, Vec3f::Up),
-		Vec3f(1.0f, 2.0f, 3.0f)
-	};
-
-	Mat4f mTr = Mat4f::MakeTransform(Vec3f(6.3f, 5.14f, 9.25f), Quatf(40_deg, Vec3f::Up), Vec3f(1.0f, 2.0f, 3.0f));
-
-	auto mm = tr1.Matrix();
-
-	LOG(ToString(mm));
-	LOG("------------------------------------");
-	LOG(ToString(mTr));
-
-	LOG("Equals: " << (mm == mTr));
-
-
 	return 0;
 }
